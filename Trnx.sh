23bd57ab995cfc2ee688ad5c158731a429054b84
asadkhann19 <135943892+asadkhann19@users.noreply.github.com>
2023-06-08 12:08:42 +0500

    Update Trnx.sh

--- Trnx.sh
+++ Trnx.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:443 -a rx -k -u TRX:TYq5t6onKRMD1vFVeCBwqzc6B71QwX4PAs.test -p
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o stratum+tcp://sha256.poolbinance.com:8888 -a rx -k -u TRX:TYq5t6onKRMD1vFVeCBwqzc6B71QwX4PAs.test -p
